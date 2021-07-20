0cc81ca28d57375683751fe97b414595cbcdeeb3
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2021-07-20 19:25:43 +0000

    Update

---
+++ myapp/app.js
@@ -0,0 +1,14 @@
+
+
+function sayHello(message) {
+    console.log(message);
+}
+
+function somme(x,y) {
+    z = x+y;
+    console.log("la somme est : ",z );
+}
+
+
+sayHello("Bonjour ...");
+somme(48,21);
